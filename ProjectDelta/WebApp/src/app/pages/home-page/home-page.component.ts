6a100154e7c6d5a12230cc8e322e50314dada47f
Michal <smephey@gmail.com>
2019-12-17 11:10:00 +0000

    Top-Nav Styling

--- ProjectDelta/WebApp/src/app/pages/home-page/home-page.component.ts
+++ ProjectDelta/WebApp/src/app/pages/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import {ApplicationService} from '../../services/application.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
